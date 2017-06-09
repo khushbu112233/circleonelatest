c615fb344581bd32331fe96eacdaf16430c13d25
Shani Shah <shani.shah.ce@gmail.com>
2017-06-09 15:16:35 +0530

    Connect option page design

---
+++ CircleOnet/app/src/main/java/com/amplearch/circleonet/Connect.java
@@ -0,0 +1,26 @@
+package com.amplearch.circleonet;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class Connect extends AppCompatActivity {
+
+    private TextView actionText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        final ActionBar actionBar = getSupportActionBar();
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.custom_actionbar);
+
+        actionText = (TextView) findViewById(R.id.mytext);
+        actionText.setText("Connect");
+    }
+}
