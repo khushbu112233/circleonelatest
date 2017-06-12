178eef6c75347d3457d4660a72e394a5bc2d1cb6
Shani Shah <shani.shah.ce@gmail.com>
2017-06-12 18:38:32 +0530

    change icons cordinator page design

---
+++ CircleOnet/app/src/main/java/com/amplearch/circleonet/CardDetail.java
@@ -0,0 +1,15 @@
+package com.amplearch.circleonet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class CardDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_detail);
+
+    }
+}
