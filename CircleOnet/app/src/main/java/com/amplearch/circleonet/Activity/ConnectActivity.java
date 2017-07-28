cbc4e45392503256e43e35d35c68ca3f5ec86005
Shani Shah <shani.shah.ce@gmail.com>
2017-07-28 17:26:06 +0530

    code to check for contact in contact list

--- CircleOnet/app/src/main/java/com/amplearch/circleonet/Activity/ConnectActivity.java
+++ CircleOnet/app/src/main/java/com/amplearch/circleonet/Activity/ConnectActivity.java
@@ -1,11 +1,16 @@
 package com.amplearch.circleonet.Activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.amplearch.circleonet.R;
 
@@ -24,6 +29,8 @@ public class ConnectActivity extends AppCompatActivity {
         imgEvents = (ImageView) findViewById(R.id.imgEvents);
         imgProfile = (ImageView) findViewById(R.id.imgProfile);
         imgConnecting = (ImageView) findViewById(R.id.fab);
+
+        contactExists(getApplicationContext(), "+91 9737032082");
 
         imgBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -114,5 +121,25 @@ public class ConnectActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public boolean contactExists(Context context, String number) {
+/// number is the phone number
+        Uri lookupUri = Uri.withAppendedPath(
+                ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
+                Uri.encode(number));
+        String[] mPhoneNumberProjection = { ContactsContract.PhoneLookup._ID, ContactsContract.PhoneLookup.NUMBER, ContactsContract.PhoneLookup.DISPLAY_NAME };
+        Cursor cur = context.getContentResolver().query(lookupUri,mPhoneNumberProjection, null, null, null);
+        try {
+            if (cur.moveToFirst()) {
+                Toast.makeText(getApplicationContext(), "Exists", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        } finally {
+            if (cur != null)
+                cur.close();
+        }
+        Toast.makeText(getApplicationContext(), "not Exists", Toast.LENGTH_LONG).show();
+        return false;
     }
 }
