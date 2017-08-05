e8e2cea7d60d20703c9e1f21427892d7aff7da78
Shani <shani.shah.ce@gmail.com>
2017-08-05 15:51:14 +0530

    + add file or file name

--- CircleOnet/app/src/main/java/com/amplearch/circleonet/Activity/EditProfileActivity.java
+++ CircleOnet/app/src/main/java/com/amplearch/circleonet/Activity/EditProfileActivity.java
@@ -1,16 +1,35 @@
 package com.amplearch.circleonet.Activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 import com.amplearch.circleonet.R;
+
+import java.io.File;
+
+import static android.R.attr.path;
 
 public class EditProfileActivity extends AppCompatActivity
 {
     private GridView gridView ;
-    String[] array ;
+    private String[] array ;
+    private EditText etAttachFile ;
+    private ImageView ivAttachFile ;
+
+    private String file_path = "";
+    private File file ;
+
+    private static final int PICKFILE_RESULT_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,6 +38,8 @@ public class EditProfileActivity extends AppCompatActivity
         setContentView(R.layout.fragment_edit_profile);
 
         gridView = (GridView)findViewById(R.id.gridView);
+        etAttachFile = (EditText)findViewById(R.id.etAttachFile);
+        ivAttachFile = (ImageView)findViewById(R.id.ivAttachFile);
 
         array = new String[]{"Accommodations","Information","Accounting","Information technology","Advertising",
                 "Insurance","Aerospace","Journalism & News","Agriculture & Agribusiness","Legal Services","Air Transportation",
@@ -28,8 +49,39 @@ public class EditProfileActivity extends AppCompatActivity
                 "Consumer Products","Retail","Education","Service","Electronics","Sports","Employment","Technology","Energy",
                 "Telecommunications","Entertainment & Recreation","Tourism","Fashion","Transportation","Financial Services",
                 "Travel","Fine Arts","Utilities","Food & Beverage","Video Game","Green Technology","Web Services","Health"};
-
         gridView.setAdapter(new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_list_item_1, array));
 
+        ivAttachFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("file/*");
+                startActivityForResult(intent,PICKFILE_RESULT_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        // TODO Auto-generated method stub
+        switch(requestCode)
+        {
+            case PICKFILE_RESULT_CODE:
+                if(resultCode == RESULT_OK)
+                {
+                    String FilePath = data.getData().getPath();
+
+//                    etAttachFile.setText(FilePath);
+
+                    File file = new File(FilePath);
+                    String file_name = file.getName();
+
+                    etAttachFile.setText(file_name);
+
+                }
+                break;
+        }
     }
 }
