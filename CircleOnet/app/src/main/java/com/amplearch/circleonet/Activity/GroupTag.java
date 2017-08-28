988fde2d8d8ff3270895f485c715372b9f8bd79d
Shani Shah <shani.shah.ce@gmail.com>
2017-08-28 11:46:47 +0530

    +grouptag create

--- CircleOnet/app/src/main/java/com/amplearch/circleonet/Activity/GroupTag.java
+++ CircleOnet/app/src/main/java/com/amplearch/circleonet/Activity/GroupTag.java
@@ -3,19 +3,24 @@ package com.amplearch.circleonet.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -58,8 +63,10 @@ public class GroupTag extends AppCompatActivity {
     private int actionBarHeight;
     ListView lstGroupTag;
     TextView txtGroup;
-    public static List<GroupModel> allTaggs ;
+    public static List<GroupModel> allTaggs;
     private GroupAdapter customAdapter;
+    ImageView imgAdd;
+    String GroupName, GroupDesc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +85,7 @@ public class GroupTag extends AppCompatActivity {
         imgConnect = (ImageView) findViewById(R.id.imgConnect);
         imgEvents = (ImageView) findViewById(R.id.imgEvents);
         imgProfile = (ImageView) findViewById(R.id.imgProfile);
+        imgAdd = (ImageView) findViewById(R.id.imgAdd);
         textView.setText("Group Tag");
         ImageView drawer = (ImageView) findViewById(R.id.drawer);
         drawer.setVisibility(View.GONE);
@@ -85,6 +93,105 @@ public class GroupTag extends AppCompatActivity {
         allTaggs = new ArrayList<>();
         //cardCount = db.getActiveNFCCount();
         new HttpAsyncTaskGroup().execute("http://circle8.asia:8081/Onet.svc/Group/Fetch");
+
+        imgAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater factory = LayoutInflater.from(GroupTag.this);
+
+                LinearLayout layout = new LinearLayout(GroupTag.this);
+                layout.setOrientation(LinearLayout.VERTICAL);
+
+                final EditText titleBox = new EditText(GroupTag.this);
+                titleBox.setHint("Group Name");
+                layout.addView(titleBox);
+
+                final EditText descriptionBox = new EditText(GroupTag.this);
+                descriptionBox.setHint("Description");
+                layout.addView(descriptionBox);
+
+                //   dialog.setView(layout);
+
+
+//text_entry is an Layout XML file containing two text field to display in alert dialog
+                final AlertDialog.Builder alert = new AlertDialog.Builder(GroupTag.this);
+                alert.setTitle("Create Group").setView(layout).setPositiveButton("Create",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                GroupName = titleBox.getText().toString();
+                                GroupDesc = descriptionBox.getText().toString();
+
+                                if (GroupName.equals("")){
+                                    Toast.makeText(getApplicationContext(), "Enetr Group Name", Toast.LENGTH_LONG).show();
+                                }
+                                else if (GroupDesc.equals("")){
+                                    Toast.makeText(getApplicationContext(), "Enetr Group Description", Toast.LENGTH_LONG).show();
+                                }
+                                else {
+                                    new HttpAsyncTaskGroupCreate().execute("http://circle8.asia:8081/Onet.svc/Group/Create");
+                                }
+                            }
+                        }).setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int whichButton) {
+     /*
+     * User clicked cancel so do some stuff
+     */
+                                dialog.cancel();
+
+                            }
+                        });
+                alert.show();
+            }
+        });
+
+        lstGroupTag.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                LayoutInflater factory = LayoutInflater.from(GroupTag.this);
+
+                LinearLayout layout = new LinearLayout(GroupTag.this);
+                layout.setOrientation(LinearLayout.VERTICAL);
+
+                final EditText titleBox = new EditText(GroupTag.this);
+                titleBox.setHint("Group Name");
+                layout.addView(titleBox);
+
+                final EditText descriptionBox = new EditText(GroupTag.this);
+                descriptionBox.setHint("Description");
+                layout.addView(descriptionBox);
+
+             //   dialog.setView(layout);
+
+
+//text_entry is an Layout XML file containing two text field to display in alert dialog
+                final AlertDialog.Builder alert = new AlertDialog.Builder(GroupTag.this);
+                alert.setTitle("Add Group").setView(layout).setPositiveButton("Add",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int whichButton) {
+
+                                Toast.makeText(getApplicationContext(), allTaggs.get(position).getGroup_Name(), Toast.LENGTH_LONG).show();
+
+                              //  Log.i("AlertDialog", "TextEntry 1 Entered " + input1.getText().toString());
+                              //  Log.i("AlertDialog", "TextEntry 2 Entered " + input2.getText().toString());
+    /* User clicked OK so do some stuff */
+                            }
+                        }).setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int whichButton) {
+     /*
+     * User clicked cancel so do some stuff
+     */
+                                dialog.cancel();
+
+                            }
+                        });
+                alert.show();
+            }
+        });
 
         imgLogo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -100,7 +207,7 @@ public class GroupTag extends AppCompatActivity {
         imgCards.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent go = new Intent(getApplicationContext(),CardsActivity.class);
+                Intent go = new Intent(getApplicationContext(), CardsActivity.class);
 
                 // you pass the position you want the viewpager to show in the extra,
                 // please don't forget to define and initialize the position variable
@@ -115,7 +222,7 @@ public class GroupTag extends AppCompatActivity {
         imgConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent go = new Intent(getApplicationContext(),CardsActivity.class);
+                Intent go = new Intent(getApplicationContext(), CardsActivity.class);
 
                 // you pass the position you want the viewpager to show in the extra,
                 // please don't forget to define and initialize the position variable
@@ -130,7 +237,7 @@ public class GroupTag extends AppCompatActivity {
         imgEvents.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent go = new Intent(getApplicationContext(),CardsActivity.class);
+                Intent go = new Intent(getApplicationContext(), CardsActivity.class);
 
                 // you pass the position you want the viewpager to show in the extra,
                 // please don't forget to define and initialize the position variable
@@ -145,7 +252,7 @@ public class GroupTag extends AppCompatActivity {
         imgProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent go = new Intent(getApplicationContext(),CardsActivity.class);
+                Intent go = new Intent(getApplicationContext(), CardsActivity.class);
 
                 // you pass the position you want the viewpager to show in the extra,
                 // please don't forget to define and initialize the position variable
@@ -158,13 +265,57 @@ public class GroupTag extends AppCompatActivity {
         });
     }
 
-    private class HttpAsyncTaskGroup extends AsyncTask<String, Void, String>
-    {
+    private class HttpAsyncTaskGroupCreate extends AsyncTask<String, Void, String> {
         ProgressDialog dialog;
 
         @Override
-        protected void onPreExecute()
-        {
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(GroupTag.this);
+            dialog.setMessage("Creating Group...");
+            //dialog.setTitle("Saving Reminder");
+            dialog.show();
+            dialog.setCancelable(false);
+            //  nfcModel = new ArrayList<>();
+            //   allTags = new ArrayList<>();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            return POST(urls[0]);
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            dialog.dismiss();
+            try {
+                if (result != null) {
+                    JSONObject jsonObject = new JSONObject(result);
+                    String Success = jsonObject.getString("Success").toString();
+                    if (Success.equals("1")) {
+                        Toast.makeText(getApplicationContext(), "Group Created..", Toast.LENGTH_LONG).show();
+                        allTaggs.clear();
+                        new HttpAsyncTaskGroup().execute("http://circle8.asia:8081/Onet.svc/Group/Fetch");
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "Group not Created..", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Not able to create Group..", Toast.LENGTH_LONG).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private class HttpAsyncTaskGroup extends AsyncTask<String, Void, String> {
+        ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
             super.onPreExecute();
             dialog = new ProgressDialog(GroupTag.this);
             dialog.setMessage("Fetching Groups...");
@@ -176,14 +327,13 @@ public class GroupTag extends AppCompatActivity {
         }
 
         @Override
-        protected String doInBackground(String... urls)
-        {
+        protected String doInBackground(String... urls) {
             return POST4(urls[0]);
         }
+
         // onPostExecute displays the results of the AsyncTask.
         @Override
-        protected void onPostExecute(String result)
-        {
+        protected void onPostExecute(String result) {
             dialog.dismiss();
             try {
                 if (result != null) {
@@ -191,16 +341,15 @@ public class GroupTag extends AppCompatActivity {
                     JSONArray jsonArray = jsonObject.getJSONArray("Groups");
                     //Toast.makeText(getContext(), jsonArray.toString(), Toast.LENGTH_LONG).show();
 
-                    if (jsonArray.length() == 0){
+                    if (jsonArray.length() == 0) {
                         lstGroupTag.setVisibility(View.GONE);
                         txtGroup.setVisibility(View.VISIBLE);
-                    }
-                    else {
+                    } else {
                         lstGroupTag.setVisibility(View.VISIBLE);
                         txtGroup.setVisibility(View.GONE);
                     }
 
-                    for (int i = 0; i < jsonArray.length(); i++){
+                    for (int i = 0; i < jsonArray.length(); i++) {
 
                         JSONObject object = jsonArray.getJSONObject(i);
                         //  Toast.makeText(getContext(), object.getString("Card_Back"), Toast.LENGTH_LONG).show();
@@ -214,24 +363,19 @@ public class GroupTag extends AppCompatActivity {
                     customAdapter = new GroupAdapter(GroupTag.this, allTaggs);
                     lstGroupTag.setAdapter(customAdapter);
                     // new ArrayAdapter<>(getApplicationContext(),R.layout.mytextview, array)
-                }
-                else
-                {
+                } else {
                     Toast.makeText(getApplicationContext(), "Not able to load Cards..", Toast.LENGTH_LONG).show();
                 }
-            }
-            catch (JSONException e) {
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public String POST4(String url)
-    {
+    public String POST4(String url) {
         InputStream inputStream = null;
         String result = "";
-        try
-        {
+        try {
             // 1. create HttpClient
             HttpClient httpclient = new DefaultHttpClient();
 
@@ -241,9 +385,9 @@ public class GroupTag extends AppCompatActivity {
 
             // 3. build jsonObject
             JSONObject jsonObject = new JSONObject();
-            jsonObject.accumulate("ProfileId", "27" );
-            jsonObject.accumulate("numofrecords", "10" );
-            jsonObject.accumulate("pageno", "1" );
+            jsonObject.accumulate("ProfileId", "27");
+            jsonObject.accumulate("numofrecords", "10");
+            jsonObject.accumulate("pageno", "1");
 
             // 4. convert JSONObject to JSON to String
             json = jsonObject.toString();
@@ -270,7 +414,62 @@ public class GroupTag extends AppCompatActivity {
 
 
             // 10. convert inputstream to string
-            if(inputStream != null)
+            if (inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return result;
+    }
+
+    public String POST(String url) {
+        InputStream inputStream = null;
+        String result = "";
+        try {
+            // 1. create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+            String json = "";
+
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("GroupDesc", GroupDesc);
+            jsonObject.accumulate("GroupName", GroupName);
+            jsonObject.accumulate("ProfileId", "27");
+
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString();
+
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
+
+            // 5. set json to StringEntity
+            StringEntity se = new StringEntity(json);
+
+            // 6. set httpPost Entity
+            httpPost.setEntity(se);
+
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+
+            // 10. convert inputstream to string
+            if (inputStream != null)
                 result = convertInputStreamToString(inputStream);
             else
                 result = "Did not work!";
@@ -284,10 +483,10 @@ public class GroupTag extends AppCompatActivity {
     }
 
     private static String convertInputStreamToString(InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
         String line = "";
         String result = "";
-        while((line = bufferedReader.readLine()) != null)
+        while ((line = bufferedReader.readLine()) != null)
             result += line;
 
         inputStream.close();
