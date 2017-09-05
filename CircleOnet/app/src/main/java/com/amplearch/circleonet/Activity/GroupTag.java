0a1c8151f0bbdf4e8fc92b3a75b692a2c8364e01
LAPTOP-1ECLBDRP\LENOVO <kajal.amplearch@gmail.com>
2017-09-05 10:37:22 +0530

    Revert "+group tag web service"
    This reverts commit 469da4a005953b07877449a3393a8034e18b3fd4.

--- CircleOnet/app/src/main/java/com/amplearch/circleonet/Activity/GroupTag.java
+++ CircleOnet/app/src/main/java/com/amplearch/circleonet/Activity/GroupTag.java
@@ -1,31 +1,21 @@
 package com.amplearch.circleonet.Activity;
 
 import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
-import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.amplearch.circleonet.Adapter.CustomAdapter;
-import com.amplearch.circleonet.Adapter.GroupAdapter;
-import com.amplearch.circleonet.Model.GroupModel;
-import com.amplearch.circleonet.Model.TestimonialModel;
 import com.amplearch.circleonet.R;
 import com.amplearch.circleonet.Utils.PrefUtils;
 import com.facebook.login.LoginManager;
@@ -35,32 +25,11 @@ import com.google.android.gms.common.api.Status;
 import com.linkedin.platform.LISessionManager;
 import com.twitter.sdk.android.Twitter;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
 public class GroupTag extends AppCompatActivity {
 
     private TextView textView;
     private ImageView imgCards, imgConnect, imgEvents, imgProfile, imgLogo;
     private int actionBarHeight;
-    ListView lstGroupTag;
-    TextView txtGroup;
-    public static List<GroupModel> allTaggs ;
-    private GroupAdapter customAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,8 +39,6 @@ public class GroupTag extends AppCompatActivity {
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.custom_actionbar);
         getSupportActionBar().setShowHideAnimationEnabled(false);
-        lstGroupTag = (ListView) findViewById(R.id.lstGroupTag);
-        txtGroup = (TextView) findViewById(R.id.txtGroup);
         textView = (TextView) findViewById(R.id.mytext);
         imgLogo = (ImageView) findViewById(R.id.imgLogo);
         imgCards = (ImageView) findViewById(R.id.imgCards);
@@ -82,9 +49,7 @@ public class GroupTag extends AppCompatActivity {
         ImageView drawer = (ImageView) findViewById(R.id.drawer);
         drawer.setVisibility(View.GONE);
         imgLogo.setImageResource(R.drawable.ic_keyboard_arrow_left_black_24dp);
-        allTaggs = new ArrayList<>();
         //cardCount = db.getActiveNFCCount();
-        new HttpAsyncTaskGroup().execute("http://circle8.asia:8081/Onet.svc/Group/Fetch");
 
         imgLogo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -157,142 +122,4 @@ public class GroupTag extends AppCompatActivity {
             }
         });
     }
-
-    private class HttpAsyncTaskGroup extends AsyncTask<String, Void, String>
-    {
-         ProgressDialog dialog;
-
-        @Override
-        protected void onPreExecute()
-        {
-            super.onPreExecute();
-            dialog = new ProgressDialog(GroupTag.this);
-            dialog.setMessage("Fetching Groups...");
-            //dialog.setTitle("Saving Reminder");
-            dialog.show();
-            dialog.setCancelable(false);
-            //  nfcModel = new ArrayList<>();
-            //   allTags = new ArrayList<>();
-        }
-
-        @Override
-        protected String doInBackground(String... urls)
-        {
-            return POST4(urls[0]);
-        }
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result)
-        {
-            dialog.dismiss();
-            try {
-                if (result != null) {
-                    JSONObject jsonObject = new JSONObject(result);
-                    JSONArray jsonArray = jsonObject.getJSONArray("Groups");
-                    //Toast.makeText(getContext(), jsonArray.toString(), Toast.LENGTH_LONG).show();
-
-                    if (jsonArray.length() == 0){
-                        lstGroupTag.setVisibility(View.GONE);
-                        txtGroup.setVisibility(View.VISIBLE);
-                    }
-                    else {
-                        lstGroupTag.setVisibility(View.VISIBLE);
-                        txtGroup.setVisibility(View.GONE);
-                    }
-
-                    for (int i = 0; i < jsonArray.length(); i++){
-
-                        JSONObject object = jsonArray.getJSONObject(i);
-                        //  Toast.makeText(getContext(), object.getString("Card_Back"), Toast.LENGTH_LONG).show();
-
-                        GroupModel nfcModelTag = new GroupModel();
-                        nfcModelTag.setGroup_ID(object.getString("group_ID"));
-                        nfcModelTag.setGroup_Name(object.getString("group_Name"));
-                        //  Toast.makeText(getContext(), object.getString("Testimonial_Text"), Toast.LENGTH_LONG).show();
-                        allTaggs.add(nfcModelTag);
-                    }
-                    customAdapter = new GroupAdapter(GroupTag.this, allTaggs);
-                    lstGroupTag.setAdapter(customAdapter);
-                   // new ArrayAdapter<>(getApplicationContext(),R.layout.mytextview, array)
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(), "Not able to load Cards..", Toast.LENGTH_LONG).show();
-                }
-            }
-            catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public String POST4(String url)
-    {
-        InputStream inputStream = null;
-        String result = "";
-        try
-        {
-            // 1. create HttpClient
-            HttpClient httpclient = new DefaultHttpClient();
-
-            // 2. make POST request to the given URL
-            HttpPost httpPost = new HttpPost(url);
-            String json = "";
-
-            // 3. build jsonObject
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.accumulate("ProfileId", "27" );
-            jsonObject.accumulate("numofrecords", "10" );
-            jsonObject.accumulate("pageno", "1" );
-
-            // 4. convert JSONObject to JSON to String
-            json = jsonObject.toString();
-
-            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
-            // ObjectMapper mapper = new ObjectMapper();
-            // json = mapper.writeValueAsString(person);
-
-            // 5. set json to StringEntity
-            StringEntity se = new StringEntity(json);
-
-            // 6. set httpPost Entity
-            httpPost.setEntity(se);
-
-            // 7. Set some headers to inform server about the type of the content
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-type", "application/json");
-
-            // 8. Execute POST request to the given URL
-            HttpResponse httpResponse = httpclient.execute(httpPost);
-
-            // 9. receive response as inputStream
-            inputStream = httpResponse.getEntity().getContent();
-
-
-            // 10. convert inputstream to string
-            if(inputStream != null)
-                result = convertInputStreamToString(inputStream);
-            else
-                result = "Did not work!";
-
-        } catch (Exception e) {
-            Log.d("InputStream", e.getLocalizedMessage());
-        }
-
-        // 11. return result
-        return result;
-    }
-
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
-
-        inputStream.close();
-        return result;
-
-    }
-
 }
--- CircleOnet/app/src/main/java/com/amplearch/circleonet/Fragments/ConnectListFragment.java
+++ CircleOnet/app/src/main/java/com/amplearch/circleonet/Fragments/ConnectListFragment.java
@@ -1,5 +1,6 @@
 package com.amplearch.circleonet.Fragments;
 
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
