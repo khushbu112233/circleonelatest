37bc8165fb2255cdb3e286668f8a02e8176c71ea
Shani Shah <shani.shah.ce@gmail.com>
2017-09-15 11:55:50 +0530

    +Create Group design

--- CircleOnet/app/src/main/java/com/circle8/circleOne/Activity/GroupsActivity.java
+++ CircleOnet/app/src/main/java/com/circle8/circleOne/Activity/GroupsActivity.java
@@ -1,15 +1,22 @@
 package com.circle8.circleOne.Activity;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.circle8.circleOne.Adapter.GroupAdapter;
@@ -35,6 +42,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class GroupsActivity extends AppCompatActivity
 {
     ArrayList<String> groupName = new ArrayList<>();
@@ -42,9 +51,12 @@ public class GroupsActivity extends AppCompatActivity
 
     ListView listView ;
     private LoginSession session;
-    private String profile_id ;
+    private String user_id ;
     public static ArrayList<GroupModel> allTaggs;
     ImageView imgBack;
+    RelativeLayout llBottom;
+    String GroupName, GroupDesc;
+    private String GroupImage = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -53,10 +65,11 @@ public class GroupsActivity extends AppCompatActivity
         setContentView(R.layout.activity_groups);
         session = new LoginSession(getApplicationContext());
         HashMap<String, String> user = session.getUserDetails();
-        profile_id = user.get(LoginSession.KEY_PROFILEID);
+        user_id = user.get(LoginSession.KEY_USERID);
         allTaggs = new ArrayList<>();
         listView = (ListView)findViewById(R.id.listView);
         imgBack = (ImageView) findViewById(R.id.imgBack);
+        llBottom = (RelativeLayout) findViewById(R.id.llBottom);
         new HttpAsyncTaskGroup().execute("http://circle8.asia:8081/Onet.svc/Group/Fetch");
        /* groupName.add("Group 1");
         groupName.add("Group 2");
@@ -73,6 +86,71 @@ public class GroupsActivity extends AppCompatActivity
             }
         });
 
+        llBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LayoutInflater factory = LayoutInflater.from(GroupsActivity.this);
+
+                LinearLayout layout = new LinearLayout(GroupsActivity.this);
+                layout.setOrientation(LinearLayout.VERTICAL);
+
+                final CircleImageView ivGroupImage = new CircleImageView(GroupsActivity.this);
+                ivGroupImage.setBorderColor(getResources().getColor(R.color.colorPrimary));
+                ivGroupImage.setBorderWidth(1);
+                ivGroupImage.setImageResource(R.drawable.usr_1);
+                int width=150;
+                int height=150;
+                LinearLayout.LayoutParams parms = new LinearLayout.LayoutParams(width,height);
+                parms.gravity = Gravity.CENTER;
+                ivGroupImage.setLayoutParams(parms);
+                layout.addView(ivGroupImage);
+
+                final EditText titleBox = new EditText(GroupsActivity.this);
+                titleBox.setHint("Group Name");
+                layout.addView(titleBox);
+
+                final EditText descriptionBox = new EditText(GroupsActivity.this);
+                descriptionBox.setHint("Description");
+                layout.addView(descriptionBox);
+
+                //   dialog.setView(layout);
+
+
+//text_entry is an Layout XML file containing two text field to display in alert dialog
+                final AlertDialog.Builder alert = new AlertDialog.Builder(GroupsActivity.this);
+                alert.setTitle("Create Group").setView(layout).setPositiveButton("Create",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                GroupName = titleBox.getText().toString();
+                                GroupDesc = descriptionBox.getText().toString();
+
+                                if (GroupName.equals("")){
+                                    Toast.makeText(getApplicationContext(), "Enetr Group Name", Toast.LENGTH_LONG).show();
+                                }
+                                else if (GroupDesc.equals("")){
+                                    Toast.makeText(getApplicationContext(), "Enetr Group Description", Toast.LENGTH_LONG).show();
+                                }
+                                else {
+                                    new HttpAsyncTaskGroupCreate().execute("http://circle8.asia:8081/Onet.svc/Group/Create");
+                                }
+                            }
+                        }).setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int whichButton) {
+     /*
+     * User clicked cancel so do some stuff
+     */
+                                dialog.cancel();
+
+                            }
+                        });
+                alert.show();
+
+            }
+        });
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
@@ -83,6 +161,108 @@ public class GroupsActivity extends AppCompatActivity
             }
         });
     }
+
+    public String POST(String url) {
+        InputStream inputStream = null;
+        String result = "";
+        try {
+            // 1. create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+            String json = "";
+
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("GroupDesc", GroupDesc);
+            jsonObject.accumulate("GroupName", GroupName);
+            jsonObject.accumulate("GroupPhoto", GroupImage);
+            jsonObject.accumulate("UserId", user_id);
+
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString();
+
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
+
+            // 5. set json to StringEntity
+            StringEntity se = new StringEntity(json);
+
+            // 6. set httpPost Entity
+            httpPost.setEntity(se);
+
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+
+            // 10. convert inputstream to string
+            if (inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return result;
+    }
+
+    private class HttpAsyncTaskGroupCreate extends AsyncTask<String, Void, String> {
+        ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(GroupsActivity.this);
+            dialog.setMessage("Creating Group...");
+            //dialog.setTitle("Saving Reminder");
+            dialog.show();
+            dialog.setCancelable(false);
+            //  nfcModel = new ArrayList<>();
+            //   allTags = new ArrayList<>();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            return POST(urls[0]);
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            dialog.dismiss();
+            try {
+                if (result != null) {
+                    JSONObject jsonObject = new JSONObject(result);
+                    String Success = jsonObject.getString("Success").toString();
+                    if (Success.equals("1")) {
+                        Toast.makeText(getApplicationContext(), "Group Created..", Toast.LENGTH_LONG).show();
+                        allTaggs.clear();
+                        new HttpAsyncTaskGroup().execute("http://circle8.asia:8081/Onet.svc/Group/Fetch");
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "Group not Created..", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Not able to create Group..", Toast.LENGTH_LONG).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     private class HttpAsyncTaskGroup extends AsyncTask<String, Void, String>
     {
@@ -169,7 +349,7 @@ public class GroupsActivity extends AppCompatActivity
 
             // 3. build jsonObject
             JSONObject jsonObject = new JSONObject();
-            jsonObject.accumulate("ProfileId", profile_id);
+            jsonObject.accumulate("UserId", user_id);
             jsonObject.accumulate("numofrecords", "50");
             jsonObject.accumulate("pageno", "1");
 
