3905980816137e107b5dd5463aba4edba417cf45
Shani Shah <shani.shah.ce@gmail.com>
2017-09-29 10:27:25 +0530

    +group delete

--- CircleOnet/app/src/main/java/com/circle8/circleOne/Adapter/GroupDetailAdapter.java
+++ CircleOnet/app/src/main/java/com/circle8/circleOne/Adapter/GroupDetailAdapter.java
@@ -2,10 +2,13 @@ package com.circle8.circleOne.Adapter;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -17,6 +20,7 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.circle8.circleOne.Activity.GroupDetailActivity;
 import com.circle8.circleOne.Activity.ImageZoom;
@@ -29,6 +33,19 @@ import com.daimajia.swipe.SwipeLayout;
 import com.daimajia.swipe.adapters.BaseSwipeAdapter;
 import com.squareup.picasso.Picasso;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -56,7 +73,9 @@ public class GroupDetailAdapter extends BaseSwipeAdapter
     private ArrayList<String> imgprofile = new ArrayList<>();
     public static ViewHolder holder;
     private SparseBooleanArray mSelectedItemsIds;
+    public static JSONArray selectedStrings = new JSONArray();
     View row;
+    int posi;
     public GroupDetailAdapter(Context applicationContext, int group_detail_items,
            ArrayList<String> name, ArrayList<String> designation, ArrayList<String> company,
            ArrayList<String> website, ArrayList<String> email, ArrayList<String> phone,
@@ -150,6 +169,8 @@ public class GroupDetailAdapter extends BaseSwipeAdapter
             public void onClick(View view)
             {
                 swipeLayout.close();
+                new HttpAsyncTaskGroupDeleteMember().execute("http://circle8.asia:8999/Onet.svc/Group/DeleteMembers");
+
             }
         });
 
@@ -161,7 +182,7 @@ public class GroupDetailAdapter extends BaseSwipeAdapter
     {
         row = convertView;
         holder = null;
-
+        selectedStrings = new JSONArray();
         holder = new ViewHolder();
         holder.personName = (TextView) row.findViewById(R.id.tvPersonName);
         holder.personDesignation = (TextView) row.findViewById(R.id.tvDesignation);
@@ -222,7 +243,7 @@ public class GroupDetailAdapter extends BaseSwipeAdapter
         holder.personDetail.setText(Address+"\n"+Company+"\n"+Website+"\n"+Email+"\n"+Phone);
 
         groupDetailModelArrayList.get(position).getImgProfile() ;
-
+         posi = position;
         if (groupDetailModelArrayList.get(position).getImgProfile().equals(""))
         {
             holder.personImage.setImageResource(R.drawable.usr_1);
@@ -269,6 +290,131 @@ public class GroupDetailAdapter extends BaseSwipeAdapter
         });
 
     }
+
+    private class HttpAsyncTaskGroupDeleteMember extends AsyncTask<String, Void, String>
+    {
+        ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            dialog = new ProgressDialog(context);
+            dialog.setMessage("Deleting Circle Member...");
+            dialog.show();
+            dialog.setCancelable(false);
+
+            String loading = "Deleting Circle Member" ;
+          //  CustomProgressDialog(loading);
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            return POST4(urls[0]);
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result)
+        {
+            dialog.dismiss();
+           // rlProgressDialog.setVisibility(View.GONE);
+
+            try
+            {
+                if (result != null)
+                {
+                    JSONObject jsonObject = new JSONObject(result);
+                    String success = jsonObject.getString("success");
+                    String message = jsonObject.getString("message");
+
+                    if (success.equalsIgnoreCase("1")){
+                        Toast.makeText(context, "Deleted suceessfully..", Toast.LENGTH_LONG).show();
+                        notifyDataSetChanged();
+                    }
+                    else {
+                        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(context, "Not able to delete member..", Toast.LENGTH_LONG).show();
+                }
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String POST4(String url) {
+        InputStream inputStream = null;
+        String result = "";
+        try {
+            // 1. create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // 2. make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+            String json = "";
+            selectedStrings.put(groupDetailModelArrayList.get(posi).getProfileid());
+
+            // 3. build jsonObject
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("GroupID", GroupDetailActivity.group_id);
+            jsonObject.accumulate("ProfileIDs", selectedStrings);
+
+            // 4. convert JSONObject to JSON to String
+            json = jsonObject.toString();
+
+            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+            // ObjectMapper mapper = new ObjectMapper();
+            // json = mapper.writeValueAsString(person);
+
+            // 5. set json to StringEntity
+            StringEntity se = new StringEntity(json);
+
+            // 6. set httpPost Entity
+            httpPost.setEntity(se);
+
+            // 7. Set some headers to inform server about the type of the content
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            // 8. Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            // 9. receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+
+            // 10. convert inputstream to string
+            if (inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        // 11. return result
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while ((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
 
     public static class ViewHolder
     {
