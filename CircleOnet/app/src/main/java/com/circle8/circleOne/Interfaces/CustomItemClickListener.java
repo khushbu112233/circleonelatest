29c9040b47bf97eb73ae6383b80eb6ee991a573b
Kajal <kajal.patadia@ample-arch.com>
2018-01-04 19:55:44 +0530

    +change

---
+++ CircleOnet/app/src/main/java/com/circle8/circleOne/Interfaces/CustomItemClickListener.java
@@ -0,0 +1,10 @@
+package com.circle8.circleOne.Interfaces;
+
+import android.view.View;
+
+/**
+ * Created by ample-arch on 12/29/2017.
+ */
+public interface CustomItemClickListener {
+    public void onItemClick(View v, int position);
+}
