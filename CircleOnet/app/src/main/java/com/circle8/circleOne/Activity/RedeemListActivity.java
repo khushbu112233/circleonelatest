c9508acfba6a89c2c2b9b3f2949f4f6f3c3187c4
Jay <jay.ample@gmail.com>
2018-02-27 15:31:33 +0530

    +reddem list

---
+++ CircleOnet/app/src/main/java/com/circle8/circleOne/Activity/RedeemListActivity.java
@@ -0,0 +1,10 @@
+package com.circle8.circleOne.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ample-arch on 2/27/2018.
+ */
+
+public class RedeemListActivity extends AppCompatActivity {
+}
